bfd08dd42803933932f698288444b12b7dbbc9df
Andrii Reznichenko <4ezeronnik@gmail.com>
2022-08-04 00:47:51 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,7 +4,13 @@ import React, { Component } from 'react';
 class App extends Component  {
 
   state = {
-    contacts: [],
+    contacts: [
+    {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+  ],
+    filter: '',
     name: '',
     number: ''
   };
@@ -36,10 +42,24 @@ class App extends Component  {
       name: '',
       number: ''
     })
-  }
+  };
+
+   changeFilter = e => {
+    this.setState({ filter: e.currentTarget.value });
+   };
+  
+    getVisibleTodos = () => {
+    const { filter, contacts} = this.state;
+    const normalizedFilter = filter.toLowerCase();
+
+    return contacts.filter(contact =>
+      contact.text.toLowerCase().includes(normalizedFilter),
+    );
+  };
 
   render() {
-    const { contacts, name, number } = this.state;
+    const { contacts, name, number, filter } = this.state;
+    
     
     return (
       <>
@@ -75,6 +95,11 @@ class App extends Component  {
           >Add contact</button>
         </form>
         <h1>Contacts</h1>
+         <label>
+         Find contacts by name
+          <input type="text" value={filter} onChange={this.changeFilter} />
+        </label>
+        
         <ul>
           {contacts.map(({ name, id, number }) => (
             <li key={id}>{name}: {number}</li>
